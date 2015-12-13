7cd1c9650bd99b30af820a47a4af05b16a055984
Dima Ilvokhin <r3tsky@gmail.com>
2015-12-13 15:20:01 +0300

    workable menu

--- term/main.lua
+++ term/main.lua
@@ -45,117 +45,169 @@ function love.load()
   enemyRes.makeTimer = enemyRes.makeTimerMax
   enemyRes.img = love.graphics.newImage('assets/images/aircraft_1b.png')
   --
+  loveframes.SetState('startMenu')
+  showStartMenu()
+end
+
+function showStartMenu()
+  local margin = 30
+  --
+  local play = loveframes.Create('button')
+  play:SetText('Play')
+  play:SetPos(
+    love.graphics.getWidth() / 2 - margin,
+    love.graphics.getHeight() / 2
+  )
+  play:SetState('startMenu')
+  play.OnClick = function(object)
+    loveframes.SetState('none')
+  end
+  --
+  local exit = loveframes.Create('button')
+  exit:SetText('Exit')
+  exit:SetPos(
+    love.graphics.getWidth() / 2 - margin,
+    love.graphics.getHeight() / 2 + margin
+  )
+  exit:SetState('startMenu')
+  exit.OnClick = function(object)
+    love.event.push('quit')
+  end
+end
+
+function showPauseMenu()
+  print('pause menu should be here')
 end
 
 function love.update(dt)
   if love.keyboard.isDown('escape') then
-    love.event.push('quit')
+    --loveframes.SetState('pauseMenu')
+    loveframes.SetState('startMenu')
   end
 
-  if love.keyboard.isDown('left', 'a') then
-    if player.x > 0 then
-      player.x = player.x - player.speed * dt
+  if loveframes.GetState() == 'none' then
+    if love.keyboard.isDown('left', 'a') then
+      if player.x > 0 then
+        player.x = player.x - player.speed * dt
+      end
+    elseif love.keyboard.isDown('right', 'd') then
+      if player.x < love.graphics.getWidth() - player.img:getWidth() then
+        player.x = player.x + player.speed * dt
+      end
+    elseif love.keyboard.isDown('up', 'w') then
+      if player.y > 0 then
+        player.y = player.y - player.speed * dt
+      end
+    elseif love.keyboard.isDown('down', 's') then
+      if player.y < love.graphics.getHeight() - player.img:getHeight() then
+        player.y = player.y + player.speed * dt
+      end
     end
-  elseif love.keyboard.isDown('right', 'd') then
-    if player.x < love.graphics.getWidth() - player.img:getWidth() then
-      player.x = player.x + player.speed * dt
-    end
-  elseif love.keyboard.isDown('up', 'w') then
-    if player.y > 0 then
-      player.y = player.y - player.speed * dt
-    end
-  elseif love.keyboard.isDown('down', 's') then
-    if player.y < love.graphics.getHeight() - player.img:getHeight() then
-      player.y = player.y + player.speed * dt
-    end
-  end
 
-  gun.canShootTimer = gun.canShootTimer - dt
-  if gun.canShootTimer < 0 then
-    gun.canShoot = true
-  end
-
-  if love.keyboard.isDown(' ', 'rctrl', 'lctrl', 'ctrl') and gun.canShoot then
-    local newBullet = {x = player.x + player.img:getWidth() / 2 - magic.bulletMargin, y = player.y,
-      img = gun.bullet}
-    table.insert(bullets, newBullet)
-    gun.sound:play()
-    gun.canShoot = false
-    gun.canShootTimer = gun.canShootTimerMax
-  end
-
-  for k, bullet in pairs(bullets) do
-    bullet.y = bullet.y - gun.speed * dt
-    if bullet.y < 0 then
-      table.remove(bullets, k)
+    gun.canShootTimer = gun.canShootTimer - dt
+    if gun.canShootTimer < 0 then
+      gun.canShoot = true
     end
-  end
-  --
-  enemyRes.makeTimer = enemyRes.makeTimer - dt
-  if enemyRes.makeTimer < 0 then
-    enemyRes.makeTimer = enemyRes.makeTimerMax
-    local rnd = math.random(magic.enemyMargin, love.graphics.getWidth() - magic.enemyMargin)
-    local newEnemy = {img = enemyRes.img, x = rnd, y = -magic.enemyMargin}
-    table.insert(enemies, newEnemy)
-  end
-  for k, enemy in pairs(enemies) do
-    enemy.y = enemy.y + enemyRes.speed * dt
-    if enemy.y > love.graphics.getHeight() + magic.enemyMargin then
-      table.remove(enemies, k)
+
+    if love.keyboard.isDown(' ', 'rctrl', 'lctrl', 'ctrl') and gun.canShoot then
+      local newBullet = {x = player.x + player.img:getWidth() / 2 - magic.bulletMargin, y = player.y,
+        img = gun.bullet}
+      table.insert(bullets, newBullet)
+      gun.sound:play()
+      gun.canShoot = false
+      gun.canShootTimer = gun.canShootTimerMax
     end
-  end
-  -- check collisions
-  for i, enemy in pairs(enemies) do
-    for j, bullet in pairs(bullets) do
-      if collide(enemy, bullet) then
+
+    for k, bullet in pairs(bullets) do
+      bullet.y = bullet.y - gun.speed * dt
+      if bullet.y < 0 then
+        table.remove(bullets, k)
+      end
+    end
+    --
+    enemyRes.makeTimer = enemyRes.makeTimer - dt
+    if enemyRes.makeTimer < 0 then
+      enemyRes.makeTimer = enemyRes.makeTimerMax
+      local rnd = math.random(magic.enemyMargin, love.graphics.getWidth() - magic.enemyMargin)
+      local newEnemy = {img = enemyRes.img, x = rnd, y = -magic.enemyMargin}
+      table.insert(enemies, newEnemy)
+    end
+    for k, enemy in pairs(enemies) do
+      enemy.y = enemy.y + enemyRes.speed * dt
+      if enemy.y > love.graphics.getHeight() + magic.enemyMargin then
+        table.remove(enemies, k)
+      end
+    end
+    -- check collisions
+    for i, enemy in pairs(enemies) do
+      for j, bullet in pairs(bullets) do
+        if collide(enemy, bullet) then
+          table.remove(enemies, i)
+          table.remove(bullets, j)
+          game.score = game.score + 1
+       end
+      end
+      if collide(enemy, player) and game.isAlive then
         table.remove(enemies, i)
-        table.remove(bullets, j)
-        game.score = game.score + 1
-     end
+        game.isAlive = false
+      end
     end
-    if collide(enemy, player) and game.isAlive then
-      table.remove(enemies, i)
-      game.isAlive = false
+    --
+    if not game.isAlive and love.keyboard.isDown('r') then
+      bullets = { }
+      enemies = { }
+      gun.canShoot = true
+      gun.canShootTimer = gun.canShootTimerMax
+      player.x = magic.startPlayerX
+      player.y = magic.startPlayerY
+      game.score = 0
+      game.isAlive = true
     end
-  end
-  --
-  if not game.isAlive and love.keyboard.isDown('r') then
-    bullets = { }
-    enemies = { }
-    gun.canShoot = true
-    gun.canShootTimer = gun.canShootTimerMax
-    player.x = magic.startPlayerX
-    player.y = magic.startPlayerY
-    game.score = 0
-    game.isAlive = true
   end
   -- gui stuff
   loveframes.update(dt)
 end
 
 function love.draw()
-  if game.isAlive then
-    love.graphics.draw(player.img, player.x, player.y)
-  else
-    love.graphics.print(
-      'Press \'R\' to restart',
-      love.graphics:getWidth() / 2 - magic.restartXMargin,
-      love.graphics:getHeight() / 2 - magic.restartYMargin
-    )
-  end
+  if loveframes.GetState() == 'none' then
+    if game.isAlive then
+      love.graphics.draw(player.img, player.x, player.y)
+    else
+      love.graphics.print(
+        'Press \'R\' to restart',
+        love.graphics:getWidth() / 2 - magic.restartXMargin,
+        love.graphics:getHeight() / 2 - magic.restartYMargin
+      )
+    end
 
-  for k, bullet in pairs(bullets) do
-    love.graphics.draw(bullet.img, bullet.x, bullet.y)
-  end
+    for k, bullet in pairs(bullets) do
+      love.graphics.draw(bullet.img, bullet.x, bullet.y)
+    end
 
-  for k, enemy in pairs(enemies) do
-    love.graphics.draw(enemy.img, enemy.x, enemy.y, magic.enemyRotate)
-  end
+    for k, enemy in pairs(enemies) do
+      love.graphics.draw(enemy.img, enemy.x, enemy.y, magic.enemyRotate)
+    end
 
-  love.graphics.setColor(255, 255, 255)
-  love.graphics.print('Score: ' .. tostring(game.score), magic.scoreX, magic.scoreY)
-  --
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.print('Score: ' .. tostring(game.score), magic.scoreX, magic.scoreY)
+  end
   loveframes.draw()
+end
+
+function love.mousepressed(x, y, button)
+  loveframes.mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+  loveframes.mousereleased(x, y, button)
+end
+
+function love.keypressed(key, unicode)
+  loveframes.keypressed(key, unicode)
+end
+
+function love.keyreleased(key)
+  loveframes.keyreleased(key)
 end
 
 function love.textinput(text)
